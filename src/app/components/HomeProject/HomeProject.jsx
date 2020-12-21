9749347d5c4f006604efd945f0525cfa1f800bfc
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-12-21 23:25:10 +0700

    The Home Page is Done:

--- src/app/components/HomeProject/HomeProject.jsx
+++ src/app/components/HomeProject/HomeProject.jsx
@@ -5,36 +5,40 @@ import MernImage from '../../assets/Images/mern.png'
 import shoty from '../../assets/Images/shoty.png'
 import done from '../../assets/Images/done.png'
 
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+
 
 import './styleHomeProject.css'
 
 import { useSpring, animated } from 'react-spring'
 import { Container } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-const trans = (x, y, s) => `perspective(70000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+const trans = (x, y, s) => `perspective(10000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 export default function HomeProject() {
     
-    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 200, friction: 70 } }))
-    const [props2, set2] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 200, friction: 70 } }))
-    const [props3, set3] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 200, friction: 70 } }))
-    const [props4, set4] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 350, friction: 70 } }))
-
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 100, friction: 70 } }))
+    const history = useHistory()
     return (
         <Container style={{marginBottom: '10rem'}}>
+            <div className='projects_text'>
+                <p className='project_text_main'> My Projects</p>
+                <h3 > My work is the bridge to various insightful digital experiences.</h3>
+            </div>
            <a
                 target="_blank" 
                 rel="noopener noreferrer" 
                 href='https://hangout-club.web.app'
             >
             <animated.div
-                onMouseMove={({ clientX: x, clientY: y }) => set3({ xys: calc(x, y) })}
-                onMouseLeave={() => set3({ xys: [0, 0, 1] })}
+                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
                 style={{
                     backgroundImage: `url(${hangoutImage})`,
-                    transform: props3.xys.interpolate(trans)
+                    transform: props.xys.interpolate(trans)
                 }}
                 className='bg hangout'
                 >
@@ -48,18 +52,15 @@ export default function HomeProject() {
                 rel="noopener noreferrer" 
                 href='https://pro-e-commerce.herokuapp.com'
             >
-                <animated.div
-                    onMouseMove={({ clientX: x, clientY: y }) => set4({ xys: calc(x, y) })}
-                    onMouseLeave={() => set4({ xys: [0, 0, 1] })}
+                <div
                     style={{
                         backgroundImage: `url(${MernImage})`,
-                        transform: props4.xys.interpolate(trans)
                     }}
                     className='bg'
                     >
                         <h2>Electronics e-commerce store</h2>
                         <button>Live now</button>
-                </animated.div>
+                </div>
 
             </a>
             <a
@@ -68,11 +69,11 @@ export default function HomeProject() {
                 href='https://github.com/MohamedAhmed122/React-native-doneWithIT'
             >
             <animated.div
-                onMouseMove={({ clientX: x, clientY: y }) => set2({ xys: calc(x, y) })}
-                onMouseLeave={() => set2({ xys: [0, 0, 1] })}
+                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
                 style={{
                     backgroundImage: `url(${done})`,
-                    transform: props2.xys.interpolate(trans)
+                    transform: props.xys.interpolate(trans)
                 }}
                 className='bg shoty'
             >
@@ -85,20 +86,26 @@ export default function HomeProject() {
                 rel="noopener noreferrer" 
                 href='https://shoty-db.web.app'
             >
-            <animated.div
-                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            <div
                 style={{
                     backgroundImage: `url(${shoty})`,
-                    transform: props.xys.interpolate(trans)
                 }}
                 className='bg'
             >
                 <h2>Clothing Store - SHOTY</h2>
                 <button>Live now</button>
-            </animated.div>
+            </div>
             </a>
-                      
+            <div style={{
+                display:'flex',
+                alignItems:'center',
+                justifyContent: 'center',
+                marginTop:'3rem'
+            }}>
+                <button onClick={()=> history.push('/project')} className='project_btn'>
+                    See More Projects <ArrowRightAltIcon />
+                </button>
+            </div>   
 
         </Container>
     )
